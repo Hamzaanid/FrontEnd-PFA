16cb4af80cc636187a41304ae825bbf1400692a2
Hamzaanid <hamzaanid.9801@gmail.com>
2023-06-07 14:01:55 +0200

    popup Websockets

--- src/Compenents/widget/popupParking.js
+++ src/Compenents/widget/popupParking.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -6,8 +7,30 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Typography from '@mui/material/Typography';
-
+const socket = io('');
 export default function Popup2({close,varOpen,attribute,navigate}) {
+   const initialState = {
+    num_available_spots: 0,
+    total_spots : 396
+  };
+  const [dataPython,setDataPython]= useState(initialState)
+  useEffect(() => {
+    socket.on('connect', () => {
+        console.log('popup Connected to server');
+      });
+    socket.on('newMessage', (data) => {
+      if(dataPython.num_available_spots !== data.num_available_spots){
+        setDataPython({
+            num_available_spots:data.num_available_spots,
+            total_spots : data.total_spots
+          });
+        }
+    });
+
+    socket.on('disconnect', () => {
+      console.log('popup Disconnected from server');
+    });
+  }, [dataPython]);
 
   return (
     <div>
@@ -32,10 +55,10 @@ export default function Popup2({close,varOpen,attribute,navigate}) {
         }>
           <DialogContentText id="alert-dialog-description">
           <Typography variant="body1" sx={{bgcolor:'#87D08D',p:1}} gutterBottom>
-            Places disponible : {126}
+            Places disponible : {dataPython.num_available_spots}
           </Typography>
           <Typography variant="body1" sx={{bgcolor:'red',p:1}}>
-            Places Occupées : {396-126}
+            Places Occupées : {dataPython.total_spots - dataPython.num_available_spots}
           </Typography>
           </DialogContentText>
         </DialogContent>
